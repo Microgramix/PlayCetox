a54f8f555e9122285fa120295e4d8589d4c0d7ad
Microgramix <office@euphorie.pt>
2025-01-03 22:11:58 +0100

    Atualizações: Melhorias no player, suporte a thumbnail e restrições

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import VideoPlayer from './components/VideoPlayer';
+import React from "react";
+import VideoPlayer from "./components/VideoPlayer";
 
 function App() {
   return (
     <div className="App">
-      
-      <VideoPlayer videoSrc="https://planocetox.com/wp-content/uploads/2025/01/VIDEO-NOVOOO-MARCO-4-DE-DEZEMBRO.mp4" />
+      {/* Configurando o vídeo com o thumbnail */}
+      <VideoPlayer
+        videoSrc="https://planocetox.com/wp-content/uploads/2025/01/VIDEO-NOVOOO-MARCO-4-DE-DEZEMBRO.mp4"
+        thumbnail="https://planocetox.com/wp-content/uploads/2025/01/thumbnail-scaled.jpg"
+        autoplay={false}
+      />
     </div>
   );
 }
