d62d731936c68497aebd8a8be9ddb7137d7ba27b
Microgramix <office@euphorie.pt>
2025-01-03 23:23:47 +0100

    Correção: Ajuste do thumbnail com object-fit: cover

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import VideoPlayer from './components/VideoPlayer';
+import React from "react";
+import VideoPlayer from "./components/VideoPlayer";
 
 function App() {
   return (
     <div className="App">
-      
-      <VideoPlayer videoSrc="https://planocetox.com/wp-content/uploads/2025/01/VIDEO-NOVOOO-MARCO-4-DE-DEZEMBRO.mp4" />
+      {/* Passa o link do vídeo e do thumbnail */}
+      <VideoPlayer
+        videoSrc="https://planocetox.com/wp-content/uploads/2025/01/VIDEO-NOVOOO-MARCO-4-DE-DEZEMBRO.mp4"
+        thumbnail="https://planocetox.com/wp-content/uploads/2025/01/thumbnail-scaled.jpg"
+      />
     </div>
   );
 }
