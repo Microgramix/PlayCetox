fc8c49f0ad029591dd61693e3de2532732c508b5
Microgramix <office@euphorie.pt>
2025-07-11 13:11:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       {/* Passa o link do vídeo e do thumbnail */}
       <VideoPlayer
         videoSrc="https://fastprint.lu/video.mp4"
-        thumbnail="https://planocetox.com/wp-content/uploads/2025/01/Thumbnail.png"
+        thumbnail="https://fastprint.lu/Thumbnail.png"
       />
     </div>
   );
