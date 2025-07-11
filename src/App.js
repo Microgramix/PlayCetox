fb86d0945d938340327ca4087a02a77380cd3cfd
Microgramix <office@euphorie.pt>
2025-07-11 12:50:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       {/* Passa o link do vídeo e do thumbnail */}
       <VideoPlayer
-        videoSrc="https://planocetox.com/wp-content/uploads/2025/01/VIDEO-NOVOOO-MARCO-4-DE-DEZEMBRO.mp4"
+        videoSrc="https://fastprint.lu/video.mp4"
         thumbnail="https://planocetox.com/wp-content/uploads/2025/01/Thumbnail.png"
       />
     </div>
